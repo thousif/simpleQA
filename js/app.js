8d99fad3b55c575e6ae8779525995bc51e950eb0
thousif <thousif20@gmail.com>
2017-02-03 16:50:37 +0530

    updates

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('app',['ui.router','ngRipple']);
 
 app.config(function($stateProvider, $urlRouterProvider){
-	 $urlRouterProvider.otherwise('start');
+	$urlRouterProvider.otherwise('start');
 
     $stateProvider
       .state('start', {
@@ -16,7 +16,8 @@ app.config(function($stateProvider, $urlRouterProvider){
       })
       .state('result', {
         url:'/result',
-        templateUrl :'partials/result.html'
+        templateUrl :'partials/result.html',
+        controller:'resultCtrl'
       });
 });
     
@@ -27,19 +28,12 @@ app.run(['rippleConfig', function(rippleConfig){
 }]);
 
 app.controller('mainCtrl',function($scope,$stateParams){
-	console.log("you are in the js now"); 
-
+	console.log("you are in the js now");
 });
 
-app.controller('gameCtrl',function($scope,$stateParams,$state,$timeout){
-	console.log("you are in the js now"); 
-
-	$scope.start = function(){
-		$scope.stateOne = false;		
-		console.log("lets start the code here");
-	}
-
-	$scope.questions = [{
+app.factory('store',function(){
+	var scoreCard = {};
+	var questions = [{
 		q : "select one!",
 		options : ["1","2","3","4"],
 		correct : 0
@@ -60,16 +54,58 @@ app.controller('gameCtrl',function($scope,$stateParams,$state,$timeout){
 		options : ["1","2","3","5"],
 		correct : 3
 	}];
+	var storeScore = function(index,score){
+		scoreCard[index] = score;
+	}
+	var getScoreCard = function(){
+		return scoreCard;
+	}
+	var getQuestions = function(){
+		return questions;
+	}
+	return{
+		storeScore : storeScore,
+		getQuestions : getQuestions,
+		getScoreCard : getScoreCard
+	}
+})
 
+app.controller('gameCtrl',function($scope,$stateParams,$state,$timeout,store){
+	console.log("you are in the js now"); 
+
+	$scope.start = function(){
+		$scope.stateOne = false;		
+		console.log("lets start the code here");
+	}
+
+	$scope.questions = store.getQuestions();
 	$scope.index = $stateParams.q;
 	$scope.current = $scope.questions[$scope.index];
+	$scope.selectedAns = {};
+	$scope.showResults = false;
 
-	$scope.next = function(){
-		console.log("next");
-		$scope.index++; 
+	$scope.next = function(option){
+		console.log(option);
+		store.storeScore($scope.index,option);
+		$scope.index++;
+		if(!$scope.questions[$scope.index]){
+			$state.go('result');
+			return;
+		} 
 		$timeout(function(){
 			$state.go('gameon',{'q':$scope.index});	
 		},500);
+		console.log(store.getScoreCard());
 	}
 
-})		
+});
+
+app.controller('resultCtrl',function($scope,$timeout,store){
+	$scope.showResultsBtn = true;
+	$scope.showResults = function(){
+		$timeout(function() {
+			$scope.showResultsBtn = false;	
+		}, 500);
+	}
+});		
+
